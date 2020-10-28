610964e487ca55fade8689b35167ffdc958e8ddf
jean <jean.flores@foundersworkshop.com>
2020-10-28 18:35:55 -0500

    refactor

--- jobs/example1Jobs.groovy
+++ jobs/example1Jobs.groovy
@@ -1,6 +1,6 @@
 String basePath = 'JobsAsCode/exampleA1'
-String repo = 'git@github.globant.com:jheison-rodriguez/jenkins-ci-cd-go-hello-world.git'
-String credentialSSH = 'gitlab_jenkins_jheison'
+String repo = 'git@github.com:JCFlores93/jenkins-ci-cd-go-hello-world.git'
+String credentialSSH = 'JCFlores93'
 
 folder(basePath) {
     description 'This example shows basic folder/job creation.'
